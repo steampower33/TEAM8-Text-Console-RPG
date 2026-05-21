60158d7dc9cc7ccb902d3113c72f7e67021d98bf
IMBOT-INC <master@i-mbot.info>
2026-05-21 19:16:23 +0900

    Init

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include "Monster/Monster.h"
 int main() {
     std::cout << "Text RPG System Initialized." << std::endl;
-
-    std::cin.get(); 
-
+    std::cin.get();
+    
     return 0;
 }
+
+
+
+
