615c1183be529cd44cb1ee84a0c4fc329f7813dc
IMBOT-INC <master@i-mbot.info>
2026-05-24 16:33:01 +0900

    [Feat] Monster Drop Item (DevItem ) To Item class

---
+++ Monster/item/DropItem.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "../../Item/IItem.h"
+#include "../data/MonsterStructs.h"
+
+class MonsterItem : public IItem
+{
+public:
+    MonsterItem(DevItem& item)
+    {
+        name = item.name;
+        gold = item.gold;
+    }
+
+    std::string GetName() const override
+    {
+        return name;
+    }
+
+    void Use(Character* character) override
+    {
+    }
+};
