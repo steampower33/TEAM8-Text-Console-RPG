eee15e5d5da09be8194f61511c5bcfa1c5f1eaf4
Noong <mw.noong@gmail.com>
2026-05-25 03:24:39 +0900

    Feat/BattleManager-RealFinish
    Feat/BattleManager-RealFinish

--- Game/GameManager.cpp
+++ Game/GameManager.cpp
@@ -1,65 +1,64 @@
-﻿#include "GameManager.h"
-
-#include "BattleManager.h"
-#include "../Character/Character.h"
-
-
-GameManager* GameManager::instance = nullptr;
-
-GameManager& GameManager::GetInstance(Character* player)
-{
-    if (instance == nullptr)
-    {
-        instance = new GameManager(player);
-    }
-    return *instance;
-}
-
-GameManager::GameManager(Character* player)
-{
-    this->player = player;
-}
-
-
-
-void GameManager::StartGame()
-{
-    while (true)
-    {
-        UpdateGameState();
-    }
-}
-
-void GameManager::UpdateGameState()
-{
-    switch (curentGameState)
-    {
-    case GameStateMachine::Menu:
-        // 시작 메뉴 처리
-        break;
-
-    case GameStateMachine::Exploring:
-        // 이동 / 이벤트
-        break;
-
-    case GameStateMachine::Battle:
-        battleManager->BattleLoop(player);
-        currentState = GameStateMachine::Exploring;
-        break;
-
-    case GameStateMachine::Shop:
-        shopManager.OpenShop(player);
-        currentState = GameState::Exploring;
-        break;
-
-    case GameState::GameOver:
-        // 종료 처리
-        break;
-    }
-}
-
-
-// void GameManager::StartBattleLoop()
+﻿// #include "GameManager.h"
+//
+// #include "BattleManager.h"
+// #include "../Character/Character.h"
+//
+// GameManager* GameManager::instance = nullptr;
+//
+// GameManager& GameManager::GetInstance(Character* player)
 // {
-//     
+//     if (instance == nullptr)
+//     {
+//         instance = new GameManager(player);
+//     }
+//     return *instance;
 // }
+//
+// GameManager::GameManager(Character* player)
+// {
+//     this->player = player;
+// }
+//
+//
+//
+// void GameManager::StartGame()
+// {
+//     while (true)
+//     {
+//         UpdateGameState();
+//     }
+// }
+//
+// void GameManager::UpdateGameState()
+// {
+//     switch (curentGameState)
+//     {
+//     case GameStateMachine::Menu:
+//         // 시작 메뉴 처리
+//         break;
+//
+//     case GameStateMachine::Exploring:
+//         // 이동 / 이벤트
+//         break;
+//
+//     case GameStateMachine::Battle:
+//         battleManager->BattleLoop(player);
+//         currentState = GameStateMachine::Exploring;
+//         break;
+//
+//     case GameStateMachine::Shop:
+//         shopManager.OpenShop(player);
+//         currentState = GameState::Exploring;
+//         break;
+//
+//     case GameState::GameOver:
+//         // 종료 처리
+//         break;
+//     }
+// }
+//
+//
+// // void GameManager::StartBattleLoop()
+// // {
+// //     
+// // }
--- Game/GameManager.h
+++ Game/GameManager.h
@@ -1,36 +1,36 @@
-﻿#pragma once
-
-
-enum class GameStateMachine
-{
-    Menu,
-    Battle,
-    Shop,
-    GameEnd
-};
-
-class BattleManager;
-class Character;
-
-
-class GameManager
-{
-    
-private:
-    static GameManager* instance;
-    GameManager(Character* player);
-
-    GameStateMachine curentGameState;
-    BattleManager* battleManager;
-    Character* player;
-    
-public:
-    static GameManager& GetInstance(Character* player);
-    
-    void StartGame();
-    void UpdateGameState();
-
-    
-    
-};
+﻿// #pragma once
+//
+//
+// enum class GameStateMachine
+// {
+//     Menu,
+//     Battle,
+//     Shop,
+//     GameEnd
+// };
+//
+// class BattleManager;
+// class Character;
+//
+//
+// class GameManager
+// {
+//     
+// private:
+//     static GameManager* instance;
+//     GameManager(Character* player);
+//
+//     GameStateMachine curentGameState;
+//     BattleManager* battleManager;
+//     Character* player;
+//     
+// public:
+//     static GameManager& GetInstance(Character* player);
+//     
+//     void StartGame();
+//     void UpdateGameState();
+//
+//     
+//     
+// };
 
