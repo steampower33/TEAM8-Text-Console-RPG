b9b2b643f05b3b87fbd035e35d229d5c838ac749
IMBOT-INC <master@i-mbot.info>
2026-05-25 19:45:52 +0900

    [feat] Shop Buy Item 추가.
    HealthPotion, Attack Boost

---
+++ Shop/function.cpp
@@ -0,0 +1,44 @@
+﻿
+#include "function.h"
+
+#include "../Character/Character.h"
+#include "../Item/HealthPotion.h"
+#include "../Item/AttackBoost.h"
+
+namespace
+{
+bool BuyHealthPotion(Character* player)
+{
+    if (player->Gold < ShopItemTable.at(ShopItems::HealthPotion).gold)
+        return false;
+
+    player->Gold -= ShopItemTable.at(ShopItems::HealthPotion).gold;
+    player->CharacterInventory.AddItem(std::make_unique<HealthPotion>());
+    return true;
+}
+
+bool BuyAttackBoost(Character* player)
+{
+    if (player->Gold < ShopItemTable.at(ShopItems::AttackBoost).gold)
+        return false;
+
+    player->Gold -= ShopItemTable.at(ShopItems::AttackBoost).gold;
+    player->CharacterInventory.AddItem(std::make_unique<AttackBoost>());
+    return true;
+}
+}
+
+bool BuyItem(ShopItems item, Character* player)
+{
+    switch (item)
+    {
+    case ShopItems::HealthPotion:
+        return BuyHealthPotion(player);
+    case ShopItems::AttackBoost:
+        return BuyAttackBoost(player);
+    }
+
+    return false;
+}
+
+bool SellItem(ShopItems item, Character* player){ return false;}
---
+++ Shop/struct.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <string>
+#include <unordered_map>
+
+enum class ShopItems
+{
+    HealthPotion,
+    AttackBoost,
+};
+
+
+struct ShopItemInfo
+{
+    std::string name;
+    int gold;
+};
+
+static const std::unordered_map<ShopItems, ShopItemInfo> ShopItemTable =
+{
+    {ShopItems::HealthPotion, {"HealthPotion", 200}},
+    {ShopItems::AttackBoost, {"AttackBoost", 100}},
+};
+
