fff5a48533a40956d072a2b52f66a23247e0a05d
Noong <mw.noong@gmail.com>
2026-05-25 18:49:31 +0900

    feat/StartEndManager-etc
    feat/StartEndManager-etc

--- Game/GameManager.cpp
+++ Game/GameManager.cpp
@@ -1,64 +1,91 @@
-﻿// #include "GameManager.h"
-//
-// #include "BattleManager.h"
-// #include "../Character/Character.h"
-//
-// GameManager* GameManager::instance = nullptr;
-//
-// GameManager& GameManager::GetInstance(Character* player)
-// {
-//     if (instance == nullptr)
-//     {
-//         instance = new GameManager(player);
-//     }
-//     return *instance;
-// }
-//
-// GameManager::GameManager(Character* player)
-// {
-//     this->player = player;
-// }
-//
-//
-//
-// void GameManager::StartGame()
-// {
-//     while (true)
-//     {
-//         UpdateGameState();
-//     }
-// }
-//
-// void GameManager::UpdateGameState()
-// {
-//     switch (curentGameState)
-//     {
-//     case GameStateMachine::Menu:
-//         // 시작 메뉴 처리
-//         break;
-//
-//     case GameStateMachine::Exploring:
-//         // 이동 / 이벤트
-//         break;
-//
-//     case GameStateMachine::Battle:
-//         battleManager->BattleLoop(player);
-//         currentState = GameStateMachine::Exploring;
-//         break;
-//
-//     case GameStateMachine::Shop:
-//         shopManager.OpenShop(player);
-//         currentState = GameState::Exploring;
-//         break;
-//
-//     case GameState::GameOver:
-//         // 종료 처리
-//         break;
-//     }
-// }
-//
-//
-// // void GameManager::StartBattleLoop()
-// // {
-// //     
-// // }
+﻿#include "GameManager.h"
+
+#include "BattleManager.h"
+#include "StartEndManager.h"
+#include "../Character/Character.h"
+
+//GameManager 싱글톤 생성 시작
+GameManager* GameManager::instance = nullptr;
+
+GameManager& GameManager::GetInstance()
+{
+    if (instance == nullptr)
+    {
+        instance = new GameManager();
+    }
+    return *instance;
+}
+
+GameManager::GameManager()
+    : curentGameState(GameStateMachine::Start)
+      , battleManager(nullptr)
+      , player(nullptr)
+{
+
+}
+
+//GameManager 싱글톤 생성 끝
+
+
+//핸들러 함수 시작
+void GameManager::StarterHandler() //초기 캐릭터 생성
+{
+    
+    StartEndManager starterManager;
+    starterManager.Start();
+    player = Character::GetInstance();
+    curentGameState = GameStateMachine::Explore;
+}
+
+void GameManager::ExploreHandler() //몬스터 조우 전 (탐험중)
+{
+    Phase phase = battleManager->BeforeBattle(*player);
+
+    if (phase == Phase::Enter)
+        curentGameState = GameStateMachine::Battle;
+
+    else if (phase == Phase::Escape)
+        curentGameState = GameStateMachine::GameEnd;
+}
+
+void GameManager::BattleHandler() // 몬스터 조우 및 전투 승리 시 다시 탐험중
+{
+    bool BattleResult = battleManager->Battle(*player);
+
+    if (BattleResult)
+        curentGameState = GameStateMachine::Explore;
+
+    else
+        curentGameState = GameStateMachine::GameEnd;
+}
+
+void GameManager::GameEndHandler() // 패배 시 게임종료
+{
+    StartEndManager starterManager;
+    starterManager.End(*player);
+}
+
+//void ShopHandler(); shopManager 완성 시 사용
+//핸들러 함수 끝
+
+
+void GameManager::Run()
+{
+    while (curentGameState != GameStateMachine::GameEnd)
+    {
+        switch (curentGameState)
+        {
+        case GameStateMachine::Start:
+            StarterHandler();
+            break;
+        case GameStateMachine::Explore:
+            ExploreHandler();
+            break;
+        case GameStateMachine::Battle:
+            BattleHandler();
+            break;
+        }
+    }
+    
+    GameEndHandler();
+}
